2381997723d01a8cb1174e6ebeedf8b18d36a18a
Rafa Campoamor <rafacampoamor@gmail.com>
2023-02-09 20:11:15 +0100

    Añadidos casos pendientes

---
+++ src/Caso2.java
@@ -0,0 +1,8 @@
+public class Caso2 {
+    public static void main(String[] args) {
+        /*
+         * Dado un Array bidimensional con números enteros, devolver la posición donde
+         * se encuentra el número más alto.
+         */
+    }
+}
---
+++ src/Caso4.java
@@ -0,0 +1,16 @@
+public class Caso4 {
+    public static void main(String[] args) {
+        /*
+         * Valida que el usuario introduce una fecha en formato ##/##/####.
+         * No es válido introducir ni el día ni el mes con un dígito. Por ejemplo
+         * 2/10/2021 o 12/3/2021.
+         * El sistema debe mostrar un mensaje con cada error que se encuentre.
+         * Por ejemplo en “q1/23-20211”
+         * -El día no puede tener letras/ deben ser números.
+         * -Falta una “/”
+         * -El símbolo “-” no es válido.
+         * -El mes es incorrecto.
+         * -El año es incorrecto.
+         */
+    }
+}
