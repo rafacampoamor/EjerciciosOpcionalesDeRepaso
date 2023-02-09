27fa7bd9d9d664ef17a449040d881990a6d76aac
Rafa Campoamor <rafacampoamor@gmail.com>
2023-02-09 20:11:27 +0100

    Añadidos Casos Pendientes

---
+++ src/Caso3.java
@@ -0,0 +1,15 @@
+public class Caso3 {
+    public static void main(String[] args) {
+        /*
+         * Pepe y Lola van a un restaurante. El camarero toma nota:
+         * Quién pide el plato.
+         * El nombre del plato.
+         * Lo que cuesta el plato.
+         * Como máximo pueden pedir 3 platos cada uno.
+         * El finalizar la comida debe presentarse un ticket con el total de la cuenta y
+         * el resumen de platos de cada uno. Por ejemplo:
+         * Pepe: 112,56€ (plato1 de Pepe, plato2 de Pepe, plato3 de Pepe).
+         * Lola: 120,02€ (plato1 de Lola, plato2 de Lola, plato3 de Lola).
+         */
+    }
+}
