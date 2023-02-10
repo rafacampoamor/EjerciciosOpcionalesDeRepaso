46b16658fc40daca160a8ddc8dfa8f76342ad22c
Rafa Campoamor <rafacampoamor@gmail.com>
2023-02-10 14:17:27 +0100

    format

--- src/Caso1.java
+++ src/Caso1.java
@@ -67,7 +67,8 @@ public class Caso1 {
     private static ArrayList<Character> InicioLetras() {
         ArrayList<Character> letras = new ArrayList<Character>();
         char[] arrayLetras = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',
-                'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','H','J','K','L','Z','X','C','V','B','N','M'};
+                'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D',
+                'F', 'G', 'H', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M' };
         for (int i = 0; i < arrayLetras.length; i++) {
             letras.add(arrayLetras[i]);
         }
