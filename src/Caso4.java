362a32330dd3eedaebd4dac7e61f0be9d6682526
Rafa Campoamor <rafacampoamor@gmail.com>
2023-02-20 18:47:55 +0100

    Caso4

--- src/Caso4.java
+++ src/Caso4.java
@@ -12,5 +12,75 @@ public class Caso4 {
          * -El mes es incorrecto.
          * -El año es incorrecto.
          */
+
+        String fecha;
+        boolean valido = false;
+
+        do {
+            fecha = Utilidades.PedirString("Dame una fecha en formato dd/mm/aaaa:");
+
+            valido = ValidarFecha(fecha);
+        } while (!valido);
+
+    }
+
+
+    /**
+     * Recibe un String y verifica que se trate de una fecha en formato dd/mm/aaaa
+     * @param fecha El String a comprobar
+     * @return devuelve un booleano comprobando si es válido o no
+     */
+    private static boolean ValidarFecha(String fecha) {
+        boolean esnumero;
+        int numero;
+        boolean valido = false;
+
+        // validar formato dd/mm/aaaa
+        if (fecha.substring(2, 3).equals("/")) {
+
+            if (fecha.substring(5, 6).equals("/")) {
+
+                esnumero = Utilidades.sonNumeros(fecha.substring(0, 2));
+                numero = Integer.parseInt(fecha.substring(0, 2));
+
+                if (esnumero && numero > 0 && numero <= 31) {
+                    esnumero = Utilidades.sonNumeros(fecha.substring(3, 5));
+                    numero = Integer.parseInt(fecha.substring(3, 5));
+
+                    if (esnumero && numero > 0 && numero <= 12) {
+                        esnumero = Utilidades.sonNumeros(fecha.substring(6));
+                        numero = Integer.parseInt(fecha.substring(6));
+                        if (esnumero && numero <= 2999) {
+                            // devolver fecha con formato
+                            System.out.println("Es el " + fecha.substring(0, 2) + " del " + fecha.substring(3, 5)
+                                    + " de " + fecha.substring(6));
+                            valido = true;
+                            return valido;
+                            // devolver error
+                        } else
+                            error();
+                        return valido;
+                    } else
+                        error();
+                    return valido;
+
+                } else
+                    error();
+                return valido;
+            } else
+                error();
+            return valido;
+        } else {
+
+            error();
+            return valido;
+        }
+    }
+
+    /**
+     * Imprime por consola un mensaje de error
+     */
+    private static void error() {
+        System.out.println("Formato de fecha inválido");
     }
 }
